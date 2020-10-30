863148226204e7212a8301f54227ae94f2141729
Mathis Hoffmann <m.hoffmann@hoffdoc.de>
2020-10-30 14:50:38 +0100

    Added a primitive GUI

---
+++ src/gui/MainFrame.java
@@ -0,0 +1,52 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTable;
+import javax.swing.JLabel;
+import java.awt.GridLayout;
+
+public class MainFrame extends JFrame {
+
+	private JPanel contentPane;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainFrame frame = new MainFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MainFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setBounds(10, 10, 550, 550);
+		setBounds(100, 100, 500, 500);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		Object[][] data = new Object[9][9];
+		table = new JTable(data, new String[] {"", "", "", "", "", "", "", "", ""});
+		table.setRowHeight(400 / 9);
+		table.setBounds(10, 11, 400, 400);
+		contentPane.add(table);
+	}
+}
