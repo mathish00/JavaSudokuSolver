97970f57d1b4c8e51d0f55e75d1c4bdcfcdc55f1
Mathis Hoffmann <m.hoffmann@hoffdoc.de>
2020-10-30 18:18:55 +0100

    Removed the gui. Unable to use the package in VS Code

--- src/solver/App.java
+++ src/solver/App.java
@@ -1,3 +1,5 @@
+package solver;
+
 public class App {
     public static void main(String[] args) throws Exception {
         Solver s = new Solver();
--- src/solver/Solver.java
+++ src/solver/Solver.java
@@ -1,3 +1,5 @@
+package solver;
+
 public class Solver {
 
     public boolean solveSudoku(int[][] sudoku) {
